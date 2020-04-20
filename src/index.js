18779894f3131a0319b73704fda35541e9456ef0
Maick Souza <maick_a_s@msn.com>
2020-04-20 08:13:27 -0300

    basic04

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
   return response.json({ message: 'Hello World' });
 });
 
 app.post('/projects', (request, response) => {
+  const body = request.body; 
 
+  return response.json(body);
 });
 
 app.put('/projects/:id', (request, response) => {
